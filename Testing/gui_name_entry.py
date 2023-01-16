6ebd72243f2536f5e45c52eed806e9f9a03cedbe
Ajay T Shaju <ajaytshaju@gmail.com>
2023-01-16 18:41:29 +0530

    almost done

--- Testing/gui_name_entry.py
+++ Testing/gui_name_entry.py
@@ -1,5 +1,5 @@
 import random, tkinter as tk
-from tkinter import filedialog, messagebox as mb
+from tkinter import PhotoImage, filedialog, messagebox as mb
 from PIL import Image, ImageTk
 from PIL.Image import Resampling
 from auto22 import generate
@@ -8,7 +8,6 @@ from auto22 import generate
 to do:
 finalize location of coordinates
 """
-
 headFont = ('Montserrat-semibold', 25)
 normalFont = ('Montserrat', 12)
 
@@ -19,16 +18,20 @@ class App(tk.Tk):
 
         self.bind("<Escape>", self.exit_window) # press 'escape' to exit the window
 
+        self.dark_mode = Image.open('images/dark_mode.png')
+        self.dark_mode_img = ImageTk.PhotoImage(self.dark_mode)    
+
         self.title("Certifly")
-        # self.iconbitmap('') # place certifly icon image here.
+        self.iconbitmap('images/icon.ico') # certifly window icon
         self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}") # "1920x1080"
         self.state('zoomed')
-
-        # Setting title & tagline
-        self.title = tk.Label(self, text='CERTIFLY', font=headFont)
+        self.title = tk.Label(self, text='CERTIFLY', font=headFont) # Setting title & tagline
         self.tagline = tk.Label(self, text='Certificates on the Go', font=normalFont)
+        self.dark_mode_button = tk.Button(self, image=self.dark_mode_img, height=30, width=30, bg='White') # command=dark_mode_switch) # dark_mode button
+        
         self.title.pack()
         self.tagline.pack()
+        self.dark_mode_button.pack(side='right', anchor='ne', padx=30)
 
         # Create a canvas Frame to hold the name textbox and buttons
         self.sideFrame = tk.LabelFrame(self)#, width=20, height=100)
@@ -43,7 +46,7 @@ class App(tk.Tk):
         self.gen_sample_button = tk.Button(self.sideFrame, text="Generate Sample", font=normalFont, command=self.generate_sample) #  button to open an image file
         self.gen_all_button = tk.Button(self.sideFrame, text="Generate All",font=normalFont, command=self.generate_all) #  button to open an image file
         self.coord_label = tk.Label(self.sideFrame, text="X: Y:",font=normalFont) # label to show the coordinates of the point clicked
-        self.info_label = tk.Label(self.sideFrame, text='',font=normalFont) # label to show info about button clicks & all
+        self.info_label = tk.Label(self.sideFrame, text='',font=normalFont, fg='green') # label to show info about button clicks & all
         ########## placing widgets ##########
         self.name_entry_title.pack(padx=30, pady=10, anchor='center')
         self.name_entry.pack(padx=30, pady=5, anchor='center')
@@ -65,6 +68,8 @@ class App(tk.Tk):
         global name_list # for checking in generate_sample() function
         names = self.name_entry.get("1.0","end").upper() # getting texts from start to end & changing to uppercase
         name_list = names.splitlines() # splitting texts by line
+        self.info_label.config(text='Names Entered')
+        self.after(5000, self.clear_label)
         print(name_list) # remove at last ......................................................................................................................
 
     def open_image(self):
@@ -75,7 +80,7 @@ class App(tk.Tk):
                 filepath = filedialog.askopenfilename()
                 self.image = Image.open(filepath)
                 width, height = 1000, 650
-                self.ratio = min(width/self.image.width, height/self.image.height)
+                self.ratio = min(width/self.image.width, height/self.image.height) # problem here===============================================
                 self.image = self.image.resize((int(self.image.width*self.ratio), int(self.image.height*self.ratio)), Resampling.BILINEAR)
                 self.photo = ImageTk.PhotoImage(self.image)
                 self.img_on_canvas = self.canvas.create_image(0, 0, image=self.photo, anchor="nw")
@@ -93,7 +98,9 @@ class App(tk.Tk):
         x = event.x/self.ratio 
         y = event.y/self.ratio
         self.coord_label.config(text=f"X: {round(x,2)}, Y: {round(y,2)}")
-        print(f"X: {x}, Y: {y}") # remove at last ................................................................................................
+        self.info_label.config(text='Coordinates Set')
+        self.after(5000, self.clear_label)
+        print(f"X: {x}, Y: {y}") # remove at last ................................................................................................................
 
     def generate_sample(self):
         sample_names = []
@@ -112,10 +119,12 @@ class App(tk.Tk):
             mb.showerror('Coordinates error', 'Please select coordinates')
         else:
             generate(name_list, x, y) # generating certificates with all given names
+            mb.showinfo('Cerificates Generated', 'All cerificates has been generated')
 
     def change_cursor(self, event): self.canvas.config(cursor="crosshair")  # to change normal cursor to crosshair(+)
     def reset_cursor(self, event): self.canvas.config(cursor="")            # when cursor is hovered over placed image
 
+    def clear_label(self): self.info_label['text'] = "" # to remove text from info label
     def exit_window(self, event=None): self.destroy() # to exit the window
 
 app = App()
--- Testing/tests.py
+++ Testing/tests.py
@@ -1,12 +1,30 @@
-from tkinter import *
+import random, tkinter as tk
+from tkinter import PhotoImage, filedialog, messagebox as mb
+from PIL import Image, ImageTk
+from PIL.Image import Resampling
+from auto22 import generate
 
-root = Tk()
-root.geometry('250x150')
+"""
+to do:
+finalize location of coordinates
+"""
+headFont = ('Montserrat-semibold', 25)
+normalFont = ('Montserrat', 12)
 
-button1 = Button(text="button1")
-button1.pack(side = BOTTOM, pady=6)
+class App(tk.Tk):
 
-button2 = Button(text="button2")
-button2.pack(side = BOTTOM, pady=3)
+    def __init__(self):
+        super().__init__()
 
-root.mainloop()
+        dark_mode = PhotoImage(file='images/dark_mode.png') # get image using PhotoImage function
+        dark_mode_img_lbl = tk.Label(image=dark_mode) # label for button event
+
+        self.title("Certifly")
+        # self.iconbitmap('') # place certifly icon image here.................................................................................................
+        self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}") # "1920x1080"
+        self.state('zoomed')
+
+        self.dark_mode_button = tk.Button(self, image=dark_mode_img_lbl) # command=dark_mode_switch
+        self.dark_mode_button.pack(side='right')
+app = App()
+app.mainloop()  
